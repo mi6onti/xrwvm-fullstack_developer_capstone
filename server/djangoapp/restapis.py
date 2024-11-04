c16bf2b375032433c322c29e2ea0cb350e91da34
Mihail Petkov <mishonti@abv.bg>
2024-11-05 00:47:28 +0200

    Update restapis.py

--- server/djangoapp/restapis.py
+++ server/djangoapp/restapis.py
@@ -6,7 +6,7 @@ from dotenv import load_dotenv
 load_dotenv()
 
 backend_url = os.getenv(
-    'backend_url',default="http://localhost:3030"
+    'backend_url', default="http://localhost:3030"
 )
 
 sentiment_analyzer_url = os.getenv(
@@ -19,7 +19,7 @@ def get_request(endpoint, **kwargs):
     params = ""
     if (kwargs):
         for key, value in kwargs.items():
-            params=params+key+"="+value+"&"
+            params = params + key + "="+value+"&"
 
     request_url = backend_url+endpoint+"?"+params
 
@@ -47,7 +47,7 @@ def analyze_review_sentiments(text):
 def post_review(data_dict):
     request_url = backend_url+"/insert_review"
     try:
-        response = requests.post(request_url,json=data_dict)
+        response = requests.post(request_url, json=data_dict)
         print(response.json())
         return response.json()
     except Exception:
